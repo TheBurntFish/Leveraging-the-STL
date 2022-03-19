77ea506eb88045ccb0d3b049b7ed908f8c566b7f
TheBurntFish <44887704+TheBurntFish@users.noreply.github.com>
2022-03-18 17:39:27 -0700

    Update main.cpp 3

--- 3/main.cpp
+++ 3/main.cpp
@@ -10,7 +10,27 @@ int main() {
     std::cout << "Cars: " << cars << "\n";
 
     // TODO: print the average price and price range
-    std::cout << "The average price is: $" << "TODO" << "\n";
+    double total = 0;
+    for (Car c : cars) {
+        total += c.price();
+    }
 
-    std::cout << "The range is: $" << "TODO" << "\n";
+    std::cout << "The average price is: $" << total/cars.size() << "\n";
+
+
+    auto maxPriceIt = std::max_element(cars.begin(), cars.end(), [](const Car& lhs, const Car& rhs) {
+        return lhs.price() < rhs.price();
+        });
+    //find max index from beginning
+    auto max = std::distance(cars.begin(), maxPriceIt);
+
+
+    auto minPriceIt = std::min_element(cars.begin(), cars.end(), [](const Car& lhs, const Car& rhs) {
+        return lhs.price() < rhs.price();
+        });
+    //find min index from beginning
+    int min = std::distance(cars.begin(), minPriceIt);
+
+    double range = cars[max].price() - cars[min].price();
+    std::cout << "The range is: $" << range << "\n";
 }
