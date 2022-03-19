d9c98dd39de1436b7abe45b8bd9a1e674ccd9d13
TheBurntFish <44887704+TheBurntFish@users.noreply.github.com>
2022-03-18 18:49:02 -0700

    Update main 4

--- 4/main.cpp
+++ 4/main.cpp
@@ -8,6 +8,7 @@
 bool anyPrime(const std::vector<int>& values);
 
 int main() {
+    
     auto integers = randomIntegers();
     std::cout << "Integers: " << integers << "\n";
     std::cout << "Are there any primes? " << (anyPrime(integers) ? "yes" : "no") << "\n";
@@ -16,6 +17,26 @@ int main() {
 bool anyPrime(const std::vector<int>& values) {
     // TODO: return true if any of the values are prime numbers
     auto result = false;
+    
+    // Go through each integer
+    for (int val : values) {
+        int count = 0;
+        if (val == 2) return true;
+        //rule out 0 and 1 and evens (2 already dealt with)
+        if (val > 2 && val % 2 != 0) {
+            
+            //prime only divisble by 1 and itself
+            //skips 1 so (count + 1) should only trigger when val == i
+            for (int i = 3; i <= val; i += 2) {
+                if (val % i == 0) {
+                    count++;
+                }
+                
+            }
+            if (count == 1) return true;
+
+        }
+    }
 
     return result;
 }
