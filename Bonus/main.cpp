8435fd859ea0623e72e6055c0e81483e8c129eb9
TheBurntFish <44887704+TheBurntFish@users.noreply.github.com>
2022-03-18 23:01:03 -0700

    Update Bonus main
    works on the tests that were small enough for me to do in my head. Not sure if it works on the larger tests.

--- Bonus/main.cpp
+++ Bonus/main.cpp
@@ -10,10 +10,45 @@ std::vector<int> createElevationMap();
 
 int main() {
     auto range = createElevationMap();
-    std::cout << "Elevation Map: " << range << "\n";
+    // test case (should be 16)
+    //std::vector<int> range = {1,0,3,6,0,1,1,0,1,0,0,1,0,1,2,1};
 
+    std::cout << "Elevation Map: " << range << "\n";
     // TODO: print out the amount of trapped water after it rains
+
+    auto peak = std::max_element(range.begin(), range.end());
+    
+    int water = 0;
+    int start = 0;
+    int last = 0;
+    //goes up a level of elevation 
+    for (int elevation = 1; elevation < *peak; elevation++) {
+        //finds index first instance of (elevation) level
+        for (int index = 0; index <= range.size(); index++) {
+            if (range[index] >= elevation) {
+                start = index;
+                break;
+            }
+        }
+        //finds index of last element with less elevation
+        for (int index = range.size()-1; index > 0; index--) {
+            if (range[index] >= elevation) {
+                last = index;
+                break;
+            }
+        }
+        //finds elevation between first and last index of selected elevation and adds water if height at index is lower than said elevation
+        for (int i = start; i <= last; i++) {
+            if (range[i] < elevation) {
+                water += 1;
+            }
+        }
+     
+    }
+
+    std::cout << "Water: " << water << "\n";
 }
+    
 
 std::vector<int> createElevationMap() {
     // generate elevation data
@@ -30,3 +65,4 @@ std::vector<int> createElevationMap() {
 
     return elevationMap;
 }
+
